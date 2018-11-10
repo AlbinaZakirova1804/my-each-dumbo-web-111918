4464cdff8171e27ac06abb59bbce1902b062ea36
Albina Zakirova <zak1804@gmail.com>
2018-11-10 16:34:19 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each
   collection = [1, 2, 3, 4]
   count = 0
   while count < collection.length-1
-  Yield
-  collection[count]
-  count += 1
+    Yield
+    collection[count]
+    count += 1
   end
 end
