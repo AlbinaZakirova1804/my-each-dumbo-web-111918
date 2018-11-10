78de21adffed4268eb582e5e2e30407a6a0fc704
Albina Zakirova <zak1804@gmail.com>
2018-11-10 16:32:40 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,11 @@
 def my_each(collection)
   #collection = [1, 2, 3, 4]
   count = 0
-  if collection.length ==0
-    "This block should not run!"
-  else
+    
   while count < collection.length-1
  
   Yield
   collection[count]
   count += 1
-end
   end
 end
